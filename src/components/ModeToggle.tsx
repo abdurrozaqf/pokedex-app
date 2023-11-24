94a0570d18662f3d2250cbe2a73b24b2583e3321
abdurrozaqf <rozaqa27@gmail.com>
2023-11-25 06:45:45 +0700

    feat: add dark mode

---
+++ src/components/ModeToggle.tsx
@@ -0,0 +1,19 @@
+import { useTheme } from "@/utils/contexts/theme-provider";
+import { Moon, Sun } from "lucide-react";
+
+export function ModeToggle() {
+  const { setTheme } = useTheme();
+
+  return (
+    <>
+      <div className="w-[5rem] bg-black/5 dark:bg-white rounded-full flex justify-between items-center p-1 h-fit shadow-inner shadow-black/25 dark:shadow-inner dark:shadow-black/50 dark:border dark:border-white/10">
+        <div onClick={() => setTheme("light")} className="cursor-pointer">
+          <Sun className="h-[2rem] w-[2rem]  bg-white/80 text-yellow-400 rounded-full p-1 opacity-100 dark:opacity-0 transition-all translate-x-0 dark:translate-x-9" />
+        </div>
+        <div onClick={() => setTheme("dark")} className="cursor-pointer">
+          <Moon className="h-[2rem] w-[2rem] cursor-pointer bg-black/80 text-white rounded-full p-1 opacity-0 dark:opacity-100 transition-all -translate-x-9 dark:translate-x-0" />
+        </div>
+      </div>
+    </>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
+import { ThemeProvider } from "./utils/contexts/theme-provider.tsx";
 import ReactDOM from "react-dom/client";
-import App from "./pages/index.tsx";
+import App from "./routes";
 import "@/styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <>
+  <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
     <App />
-  </>
+  </ThemeProvider>
 );
