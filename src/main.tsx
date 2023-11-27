58afe349851a541967ee6d135ecd274b68ca07c2
abdurrozaqf <rozaqa27@gmail.com>
2023-11-28 01:20:35 +0700

    feat: add toaster

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { ThemeProvider } from "./utils/contexts/theme-provider.tsx";
 import ReactDOM from "react-dom/client";
 import App from "./routers/index.tsx";
 import "@/styles/index.css";
+import { Toaster } from "./components/ui/toaster.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
     <App />
+    <Toaster />
   </ThemeProvider>
 );
